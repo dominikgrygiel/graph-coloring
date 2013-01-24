9ad3fddbdadbaa34ebf11587d02375c930b959f3
taya <info@blackbyte.pl>
2013-01-24 19:40:28 +0100

    brute giving worst chromatic.

--- coloring-benchmark.js
+++ coloring-benchmark.js
@@ -6,8 +6,8 @@ for(var i = 1; i <= 100; i++) {
   graph.makeRandomWithSaturation();
 
   var brute = graph.bruteColorize(), lf = graph.lfColorize(), sl = graph.slColorize(), slf = graph.slfColorize();
-  if((brute > lf) || (brute > sl) || (brute > slf)) {
+  ////if((brute > lf) || (brute > sl) || (brute > slf)) {
     console.log(i + ": " + brute + " " + lf + " " + sl + " " + slf);
-  }
+ // }
 }
 
