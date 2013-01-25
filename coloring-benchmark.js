b4dbbcff09896aa032f5a78ce22c995231faaf15
Dominik Grygiel <dominik.grygiel@gmail.com>
2013-01-25 19:20:51 +0100

    rs colorize

--- coloring-benchmark.js
+++ coloring-benchmark.js
@@ -1,23 +1,26 @@
 var _ = require("./lib/underscore-min.js"), microtime = require('microtime'),
     UndirectedGraph = require("./lib/undirected_graph.js").UndirectedGraph;
 
-process.stdout.write("V;brute;;LF;;SL;;SLF;;\n");
+process.stdout.write("V;brute;;RS;;LF;;SL;;SLF;;\n");
 
 for(var i = 1; i <= 100; i++) {
   var graph = new UndirectedGraph(i), start;
-  graph.makeRandomWithSaturation(80);
+  graph.makeRandomWithSaturation();
 
   process.stdout.write(i + ";");
   start = microtime.now();
-  process.stdout.write(graph.bruteColorize() + ";" + (microtime.now() - start) / 1000 + ";");
+  process.stdout.write(graph.bruteColorize() + ";" + (microtime.now() - start) / 1000000 + ";");
 
   start = microtime.now();
-  process.stdout.write(graph.lfColorize() + ";" + (microtime.now() - start) / 10000 + ";");
+  process.stdout.write(graph.rsColorize() + ";" + (microtime.now() - start) / 10000000 + ";");
 
   start = microtime.now();
-  process.stdout.write(graph.slColorize() + ";" + (microtime.now() - start) / 10000 + ";");
+  process.stdout.write(graph.lfColorize() + ";" + (microtime.now() - start) / 10000000 + ";");
 
   start = microtime.now();
-  process.stdout.write(graph.slfColorize() + ";" + (microtime.now() - start) / 10000 + "\n");
+  process.stdout.write(graph.slColorize() + ";" + (microtime.now() - start) / 10000000 + ";");
+
+  start = microtime.now();
+  process.stdout.write(graph.slfColorize() + ";" + (microtime.now() - start) / 10000000 + "\n");
 }
 
